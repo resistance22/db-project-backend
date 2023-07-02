f89496888d221aff92ddff6c2ffe9738e7d7c6e2
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-02 18:06:55 +0330

    [feat]:Added user entity

---
+++ src/Enitity/User.entity.ts
@@ -0,0 +1,10 @@
+class User {
+    role: number
+    password: string
+    first_name: string
+    last_name: string
+    email: string
+    phone_number: string
+    created_at: string
+    updated_at: string
+}
