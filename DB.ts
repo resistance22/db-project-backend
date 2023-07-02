badfb04b8a5e58e8d65027e6dcc3cc88667b2f78
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-02 19:06:04 +0330

    [ref]

---
+++ DB.ts
@@ -0,0 +1,5 @@
+import { Pool, Client } from 'pg'
+
+const pool = new Pool()
+
+export default pool;
--- types/User.d.ts
+++ types/User.d.ts
@@ -13,15 +13,11 @@ declare global {
         email: string
         phone_number: string
         password: string
-        email_confirmed?: boolean
-        blocked?: boolean
       }
       interface IUser extends NewUser {
         id: string
-        registered_at: string
+        created_at: string
         updated_at: string
-        email_confirmed: boolean
-        blocked: boolean
       }
     }
 
@@ -54,19 +50,12 @@ declare global {
       type register = (usecase: UseCases.IUserRegister) => IRoute
     }
 
-
     interface IUserRepository {
-      hydrate: (sampleData: UserNS.DTO.NewUser[]) => Promise<void>
-      truncate: () => Promise<void>
       insertNewUser: (user: UserNS.DTO.NewUser) => Promise<UserEntitiy>
       fetchUserByPhoneNumber: (phone_number: string) => Promise<UserEntitiy | null>
       fetchUserByEmail: (email: string) => Promise<UserEntitiy | null>
       fetchUserByIdentifier: (identifier: string) => Promise<UserEntitiy | null>
       fetchUserByID: (id: string) => Promise<UserEntitiy | null>
-      insertRefreshToken: (id: string, refreshToken: string) => Promise<void>
-      insertEmailConfirmationCode: (email: string, code: string) => Promise<string>
-      checkEmailCode: (email: string, code: string) => Promise<{ success: true } | { success: false, error: string }>
-      confirmUserEmail: (email: string) => Promise<UserEntitiy | null>
     }
 
     interface IUserUtils {
