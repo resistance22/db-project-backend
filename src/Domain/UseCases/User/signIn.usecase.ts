4bca4f37c0f245b3380b6ccaf934378df9fc1cf2
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-04 11:52:37 +0330

    [ref]:Commented refresh token db save

--- src/Domain/UseCases/User/signIn.usecase.ts
+++ src/Domain/UseCases/User/signIn.usecase.ts
@@ -4,30 +4,33 @@ import { WronCrudentialsError } from "@errors/WronCrudentials"
 export class UserSignIn implements UserNS.UseCases.IUserSignIn {
     userRepo: UserNS.IUserRepository
     userUtils: UserNS.IUserUtils
-    constructor(userRepo: UserNS.IUserRepository, userUtils: UserNS.IUserUtils){
+    constructor(userRepo: UserNS.IUserRepository, userUtils: UserNS.IUserUtils) {
         this.userRepo = userRepo
         this.userUtils = userUtils
     }
 
-    async execute({identifier, password}: { identifier: string, password: string }){
-       const foundUser = await  this.userRepo.fetchUserByIdentifier(identifier)
-       if(foundUser === null){
+    async execute({ identifier, password }: { identifier: string, password: string }) {
+        const foundUser = await this.userRepo.fetchUserByIdentifier(identifier)
+        if (foundUser === null) {
             throw new WronCrudentialsError("Wrong Crudentials")
-       }
+        }
 
-       const isPasswordOK = await this.userUtils.checkPassword(foundUser.password, password)
+        const isPasswordOK = await this.userUtils.checkPassword(foundUser.password, password)
 
-       if(!isPasswordOK){
+        if (!isPasswordOK) {
             throw new WronCrudentialsError("Wrong Crudentials")
-       }
+        }
 
-       const { password: _ , ...usrWithoutPass} = foundUser
-       const refreshToken = await this.userUtils.makeJWT(usrWithoutPass, process.env.ACCESS_TOKEN_SECRET as string)
-       const accessToken = await this.userUtils.makeJWT(usrWithoutPass, process.env.REFRESH_TOKEN_SECRET as string)
-       this.userRepo.insertRefreshToken(foundUser, refreshToken)
-       return {
-        accessToken,
-        refreshToken
-       }
+        const { password: _, ...usrWithoutPass } = foundUser
+        const refreshToken = await this.userUtils.makeJWT(usrWithoutPass, process.env.ACCESS_TOKEN_SECRET as string)
+        const accessToken = await this.userUtils.makeJWT(usrWithoutPass, process.env.REFRESH_TOKEN_SECRET as string)
+
+        // for later refreshToken functionality
+        // this.userRepo.insertRefreshToken(foundUser, refreshToken)
+
+        return {
+            accessToken,
+            refreshToken
+        }
     }
 }
