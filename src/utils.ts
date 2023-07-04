5e5321e7648a6594edf501f3ced5218e52bce181
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-04 13:29:16 +0330

    [feat]: Added first admin account setup

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { ROLES } from './Enums'
+import { hash } from 'bcrypt'
 import fs from 'fs'
 import path from 'path'
 import { Client } from 'pg'
@@ -7,5 +9,11 @@ export const generateTables = async () => {
     const client = new Client()
     await client.connect()
     await client.query(sql);
+    const count = await client.query('SELECT COUNT(user_id) FROM panel_user')
+    if (count.rows[0].count == 0) {
+        const hashedPass = await hash(process.env.ADMIN_PASSWORD as string, 10)
+        const values = [ROLES.ADMIN, process.env.ADMIN_FIRST_NAME, process.env.ADMIN_LAST_NAME, process.env.ADMIN_EMAIL, process.env.ADMIN_PHONE_NUMBER, hashedPass]
+        await client.query('INSERT INTO panel_user(role, first_name, last_name, email, phone_number, password) VALUES($1, $2, $3, $4, $5, $6);', values)
+    }
     await client.end()
 }
