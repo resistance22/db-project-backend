ea78b3915fb83606a3d9bfb9cef11ba9fe7447d7
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-05 18:44:47 +0330

    [feat]:Added CostRepo interface

---
+++ types/Cost.d.ts
@@ -0,0 +1,31 @@
+import { RequestHandler } from "express"
+import { Cost as CostEntity } from "@entities/Cost.entity"
+export { }
+
+declare global {
+  declare namespace CostNS {
+    declare namespace DTO {
+      interface NewCost {
+        title: string
+      }
+      interface IUser extends NewCost {
+        id: string
+        creator_user_id: number
+        created_at: string
+        updated_at: string
+      }
+    }
+
+    declare namespace UseCases {
+
+    }
+
+    interface ICostRepository {
+      insertNewCost(cost: CostNS.DTO.NewCost, creator_id: number): Promise<CostEntity>
+      getCostByID(id: number): Promise<CostEntity>
+    }
+
+    interface IUserEntity extends UserEntitiy { }
+
+  }
+}
