4a33866d2695d4e0320efedd167b8f689e2383ba
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-05 16:09:32 +0330

    [feat]:Added product table

--- src/DDL.sql
+++ src/DDL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS panel_user (
-	user_id serial PRIMARY KEY,
+	user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	role smallint NOT NULL,
 	first_name varchar(100) NOT NULL,
 	last_name varchar(100) NOT NULL,
@@ -10,6 +10,17 @@ CREATE TABLE IF NOT EXISTS panel_user (
 	password char(70)
 );
 
+
+CREATE TABLE IF NOT EXISTS product (
+	product_code INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	title varchar(250) NOT NULL,
+	creator_user_id INT NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	CONSTRAINT fk_author
+		FOREIGN KEY (creator_user_id)
+			REFRENCES panel_user(user_id)
+);
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
 	user_id serial PRIMARY KEY,
