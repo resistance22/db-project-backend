09f927d4079505ecc5f29f15bb904ce432646960
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-06 16:49:28 +0330

    [feat]:Implemented CostRepo

---
+++ src/repos/cost.repo.ts
@@ -0,0 +1,27 @@
+import { Pool, QueryResult } from 'pg'
+import { Cost } from "@entities/Cost.entity"
+import { hash } from 'bcrypt'
+
+export class CostRepo implements CostNS.ICostRepository {
+  connection: Pool = new Pool()
+  async getCostByID(id: number) {
+    const sql = 'SELECT * FROM cost_type WHERE id=$1;'
+    const values = [id]
+    const client = await this.connection.connect()
+    const res: QueryResult<Cost> = await client.query(sql, values)
+    await client.release()
+    if (res.rowCount === 0) {
+      return null
+    }
+    return res.rows[0];
+  }
+
+  async insertNewCost(cost: CostNS.DTO.NewCost, creator_id: number) {
+    const sql = 'INSERT INTO cost_type(creator_user_id, title) VALUES($1, $2) RETURNING *;'
+    const values = [creator_id, cost.title]
+    const client = await this.connection.connect()
+    const res: QueryResult<Cost> = await client.query(sql, values)
+    await client.release()
+    return res.rows[0];
+  }
+}
