c44d01f6f1af4c031cd0d09c51bfcad9ecb8b490
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-07 23:56:09 +0330

    [ref]: Changed order

--- src/domain/usecases/cost/add.usecase.ts
+++ src/domain/usecases/cost/add.usecase.ts
@@ -5,13 +5,13 @@ export class AddCostUseCase implements CostNS.UseCases.IAddCost {
   constructor(private costRepo: CostNS.ICostRepository) { }
 
   async execute(costData: CostNS.DTO.NewCost, creator_id: number) {
-    const addedCost = await this.costRepo.insertNewCost(costData, creator_id)
     const foundCost = await this.costRepo.getCostByTitle(costData.title)
 
     if (foundCost != null) {
       throw new DuplicateError("title", "عنوان باید یکتا باشد")
     }
 
+    const addedCost = await this.costRepo.insertNewCost(costData, creator_id)
     if (addedCost === null) {
       throw Error("SomeThing went wrong!")
     } else {
