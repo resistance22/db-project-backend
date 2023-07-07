d8c3a8a565f9d3b0b20095325d14cb8e7e522a6a
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-08 02:46:51 +0330

    [ref]: Used usecase instead of repo

--- src/controllers/cost/getOne.controller.ts
+++ src/controllers/cost/getOne.controller.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from "express"
 import { HTTPError } from "@assets/HTTPError"
 import { CostRepo } from "@/repos/cost.repo"
+import { GetOneCostUseCase } from "@/domain/usecases/cost/getOne.useCase"
 
 export const getOneCostController = async (req: Request, res: Response, next: NextFunction) => {
   const costRepo = new CostRepo()
+  const getOneCostUseCase = new GetOneCostUseCase(
+    costRepo,
+  )
   try {
-    const foundCost = await costRepo.getCostByID(parseInt(req.params.costID));
+    const foundCost = await getOneCostUseCase.execute(parseInt(req.params.costID));
     return res.json(foundCost)
   } catch (e: any) {
     if (e.name == "NotFoundError") {
