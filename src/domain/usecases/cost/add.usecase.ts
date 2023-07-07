25678769e5d47fda8066d70772065906fb5ca61c
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-07 23:25:12 +0330

    [ref]:Added unique check

--- src/domain/usecases/cost/add.usecase.ts
+++ src/domain/usecases/cost/add.usecase.ts
@@ -1,12 +1,19 @@
+import { DuplicateError } from "@/domain/errors/DuplicateError"
 
 export class AddCostUseCase implements CostNS.UseCases.IAddCost {
   userRepo: CostNS.ICostRepository
   constructor(private costRepo: CostNS.ICostRepository) { }
 
   async execute(costData: CostNS.DTO.NewCost, creator_id: number) {
-    const addedCost = await this.costRepo.insertNewCost(costData, creator_id);
+    const addedCost = await this.costRepo.insertNewCost(costData, creator_id)
+    const foundCost = await this.costRepo.getCostByTitle(costData.title)
+
+    if (foundCost != null) {
+      throw new DuplicateError("title", "عنوان باید یکتا باشد")
+    }
+
     if (addedCost === null) {
-      throw Error("Test")
+      throw Error("SomeThing went wrong!")
     } else {
       return addedCost
     }
