611340fb86e408868df9c17d0b419dc698aa499c
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-07 13:40:34 +0330

    [feat]:Added cors & cookie-parser middlewares

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express from "express"
 import { ErrorHandlerMiddleWare } from '@middlewares/ErrorHandler'
 import { middlewareFactory } from "@middlewares/index"
 import { authRouter } from "@routes/auth/index"
-import dotenv from "dotenv"
 import { generateTables } from "./utils"
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import dotenv from "dotenv"
 
 async function main() {
   dotenv.config()
@@ -15,6 +17,8 @@ async function main() {
   })
   app.use(express.static("public"))
   app.use(express.json())
+  app.use(cookieParser())
+  app.use(cors())
   app.use(reqLogger)
 
 
