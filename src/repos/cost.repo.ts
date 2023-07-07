96bb0b3b721895e62d8c5a414d4d4e922e617aa7
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-08 00:00:05 +0330

    [feat]:Implemented getCostList

--- src/repos/cost.repo.ts
+++ src/repos/cost.repo.ts
@@ -4,6 +4,7 @@ import { hash } from 'bcrypt'
 
 export class CostRepo implements CostNS.ICostRepository {
   connection: Pool = new Pool()
+
   async getCostByID(id: number) {
     const sql = 'SELECT * FROM cost_type WHERE id=$1;'
     const values = [id]
@@ -43,6 +44,27 @@ export class CostRepo implements CostNS.ICostRepository {
     } finally {
       await client.release()
     }
+  }
 
+  async getCostList(query: CostNS.getCostQuery = {}) {
+    const sort_by = query?.sort_by || "created_at"
+    const sort_order = query?.sort_order || "DESC"
+    const page = query?.page || 1
+    const per_page = query?.per_page || 20
+    const offset = (page - 1) * per_page
+    const values = [per_page, offset]
+
+    const sql = `SELECT * FROM cost_type ORDER BY ${sort_by} ${sort_order} LIMIT $1 OFFSET $2`
+    const countSQL = 'SELECT COUNT(user_id) FROM cost_type'
+    const client = await this.connection.connect()
+    const res: QueryResult<Cost> = await client.query(sql, values);
+    const count: QueryResult<{ count: string }> = await client.query(countSQL)
+    await client.release()
+    return {
+      result: res.rows,
+      meta: {
+        total: parseInt(count.rows[0].count)
+      }
+    };
   }
 }
