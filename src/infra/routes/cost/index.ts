ca7509adfc27bdf774f22704b1d35266b9eb211d
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-08 01:12:35 +0330

    [feat]:Added updateCostRouter

--- src/infra/routes/cost/index.ts
+++ src/infra/routes/cost/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { addNewCostRouter } from "@infra/routes/cost/addNewCost.route"
 import { getCostsListRouter } from "@infra/routes/cost/getCostList.route"
+import { updateCostRouter } from "@infra/routes/cost/updateCost.route"
 
 export const costRouter = Router()
 
 costRouter.use('/costs', addNewCostRouter)
 costRouter.use('/costs', getCostsListRouter)
+costRouter.use('/costs', updateCostRouter)
