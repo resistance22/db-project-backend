0264b4007c28021f23a90f4785b569ca643ce8d8
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-08 01:10:53 +0330

    [feat]:Added updateCostController

---
+++ src/controllers/cost/updateCost.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express"
+
+import { UserUtils } from '@infra/UserUtils'
+import { HTTPError } from "@/assets/HTTPError"
+import { CostRepo } from "@/repos/cost.repo"
+import { UpdateCostUseCase } from "@/domain/usecases/cost/update.usecase"
+
+export const addNewCostController = async (req: Request, res: Response, next: NextFunction) => {
+  const costRepository = new CostRepo()
+  const addCostUseCase = new UpdateCostUseCase(
+    costRepository,
+  )
+
+
+  try {
+    const updatedCost = await addCostUseCase.execute(req.body, parseInt(req.params.costID))
+    return res.json(updatedCost)
+  } catch (e: any) {
+    if (e.name == "DuplicateError") {
+      const err = new HTTPError(409, e.message, [])
+      return next(err)
+    }
+    return next(e)
+  }
+}
