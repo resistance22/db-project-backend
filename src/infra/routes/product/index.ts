5577d061cc65e6fafdfd5da0d4c45510fc63a6a6
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-09 01:12:50 +0330

    [feat]:Added getByID Route

--- src/infra/routes/product/index.ts
+++ src/infra/routes/product/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { addNewProductRouter } from "@infra/routes/product/add.route"
 import { getProductsListRouter } from "@infra/routes/product/getList.route"
+import { getProductByIDRouter } from "@infra/routes/product/getByID.route"
 
 
 export const prodcutRouter = Router()
 
 prodcutRouter.use('/products', addNewProductRouter)
 prodcutRouter.use('/products', getProductsListRouter)
+prodcutRouter.use('/products', getProductByIDRouter)
 
