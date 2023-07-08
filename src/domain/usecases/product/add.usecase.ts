8b030810e7a63a0d62a2328a77757611c0baea86
Amin Foroutann <aminforoutan7@gmail.com>
2023-07-08 21:20:08 +0330

    [feat]:Implemented AddCostUseCase

---
+++ src/domain/usecases/product/add.usecase.ts
@@ -0,0 +1,16 @@
+import { Product } from "@/domain/enitity/Product.entity"
+import { DuplicateError } from "@/domain/errors/DuplicateError"
+
+export class AddCostUseCase implements ProductNS.UseCases.IAddProduct {
+  constructor(private productRepo: ProductNS.IProductRepository) { }
+
+  async execute(cosAmountData: ProductNS.DTO.NewProduct, creator_id: number) {
+    const insertedProduct = await this.productRepo.insertNewProduct(cosAmountData, creator_id)
+
+    if (insertedProduct === null) {
+      throw Error("SomeThing went wrong!")
+    } else {
+      return insertedProduct
+    }
+  }
+}
